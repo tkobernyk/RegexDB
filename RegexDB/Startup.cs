4f942f5ca08cad7ed4621adc16ec6d648dcd4e27
tkobernyk <taras.kobernyk@gmail.com>
2017-03-08 13:24:14 +0200

    Created web solution for RegexDB

---
+++ RegexDB/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(RegexDB.Startup))]
+
+namespace RegexDB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
